a6aad6279bdf89a9b7b5ab8fed920b8f420d1b32
tanwei mac <vnicers@gmail.com>
2017-09-20 23:38:51 +0800

    将 jetty 插件替换成 gretty

--- 02TodoApp/src/main/java/com/vnicers/todo/web/ToDoServlet.java
+++ 02TodoApp/src/main/java/com/vnicers/todo/web/ToDoServlet.java
@@ -1,4 +1,52 @@
 package com.vnicers.todo.web;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+
 public class ToDoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public String getInitParameter(String name) {
+        return super.getInitParameter(name);
+    }
+
+    @Override
+    public Enumeration<String> getInitParameterNames() {
+        return super.getInitParameterNames();
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return super.getServletConfig();
+    }
+
+    @Override
+    public ServletContext getServletContext() {
+        return super.getServletContext();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
 }
